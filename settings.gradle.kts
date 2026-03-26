f33e87de4f0ef13d0ae3601255ae23c2f4336e5a
magisk317 <code@mail.usdt.edu.kg>
2026-03-26 13:50:52 +0800

    chore: add magisk-ui-kit submodule

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,4 +8,3 @@ pluginManagement {
 }
 
 include(":app", ":runtime", ":core", ":smscode-core:smscode-xposed-core", ":smscode-core:smscode-domain", ":magisk-ui-kit")
-project(":magisk-ui-kit").projectDir = file("../magisk-ui-kit")
