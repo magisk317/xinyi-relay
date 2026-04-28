6d0fc18b8cd0ce2da79558431b142f416d78cb3b
magisk317 <code@mail.usdt.edu.kg>
2026-04-29 01:49:15 +0800

    chore(renovate): enable gradle wrapper execution and remove git author

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -3,5 +3,5 @@ module.exports = {
   repositories: ["magisk317/xinyi-relay"],
   onboarding: false,
   requireConfig: "required",
-  gitAuthor: "magisk317 <93979778+magisk317@users.noreply.github.com>",
+  allowedUnsafeExecutions: ["gradleWrapper"],
 };
