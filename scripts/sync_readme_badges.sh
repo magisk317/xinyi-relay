d95a8d0a925237654c1c53ff30c2d437b180ec9a
magisk317 <code@mail.usdt.edu.kg>
2026-05-27 22:31:11 +0800

    fix(scripts): dynamically parse gradle version in readme sync script

--- scripts/sync_readme_badges.sh
+++ scripts/sync_readme_badges.sh
@@ -49,11 +49,18 @@ if [[ -z "$XPOSED_API_VERSION" ]]; then
   echo "WARN: missing xposed/libxposed version in $TOML_FILE; defaulting to $XPOSED_API_VERSION" >&2
 fi
 
+GRADLE_VERSION=$(sed -nE 's/^distributionUrl=.*gradle-([0-9a-zA-Z.-]+)-(bin|all)\.zip/\1/p' gradle/wrapper/gradle-wrapper.properties)
+if [[ -z "$GRADLE_VERSION" ]]; then
+  echo "WARN: missing gradle version in gradle-wrapper.properties; defaulting to 9.5.1" >&2
+  GRADLE_VERSION="9.5.1"
+fi
+
 KOTLIN_BADGE=$(badge_escape "$KOTLIN_VERSION")
 COMPOSE_BADGE=$(badge_escape "$COMPOSE_BOM_VERSION")
 AGP_BADGE=$(badge_escape "$AGP_VERSION")
+GRADLE_BADGE=$(badge_escape "$GRADLE_VERSION")
 
-SECOND_BADGE_LINE="[![Kotlin](https://img.shields.io/badge/Kotlin-${KOTLIN_BADGE}-7F52FF?style=flat-square&logo=kotlin&logoColor=white)](https://kotlinlang.org) [![Jetpack Compose](https://img.shields.io/badge/Jetpack_Compose-BOM_${COMPOSE_BADGE}-4285F4?style=flat-square&logo=android&logoColor=white)](https://developer.android.com/jetpack/compose) [![Gradle](https://img.shields.io/badge/Gradle-9.5.0--nightly-02303A?style=flat-square&logo=gradle&logoColor=white)](https://gradle.org) [![AGP](https://img.shields.io/badge/AGP-${AGP_BADGE}-3DDC84?style=flat-square&logo=gradle&logoColor=white)](https://developer.android.com/studio/releases/gradle-plugin) [![Min SDK](https://img.shields.io/badge/Min_SDK-${MIN_SDK_VERSION}-brightgreen?style=flat-square&logo=android)](https://developer.android.com/about/versions) [![Target SDK](https://img.shields.io/badge/Target_SDK-${TARGET_SDK_VERSION}-blue?style=flat-square&logo=android)](https://developer.android.com/about/versions) [![Xposed API](https://img.shields.io/badge/Xposed_API-${XPOSED_API_VERSION}-orange?style=flat-square)](https://github.com/rovo89/XposedBridge) [![Telegram](https://img.shields.io/badge/Telegram-Group-2CA5E0?style=flat-square&logo=telegram&logoColor=white)](https://t.me/+NR2QaQ4dlEgxYmNl)"
+SECOND_BADGE_LINE="[![Kotlin](https://img.shields.io/badge/Kotlin-${KOTLIN_BADGE}-7F52FF?style=flat-square&logo=kotlin&logoColor=white)](https://kotlinlang.org) [![Jetpack Compose](https://img.shields.io/badge/Jetpack_Compose-BOM_${COMPOSE_BADGE}-4285F4?style=flat-square&logo=android&logoColor=white)](https://developer.android.com/jetpack/compose) [![Gradle](https://img.shields.io/badge/Gradle-${GRADLE_BADGE}-02303A?style=flat-square&logo=gradle&logoColor=white)](https://gradle.org) [![AGP](https://img.shields.io/badge/AGP-${AGP_BADGE}-3DDC84?style=flat-square&logo=gradle&logoColor=white)](https://developer.android.com/studio/releases/gradle-plugin) [![Min SDK](https://img.shields.io/badge/Min_SDK-${MIN_SDK_VERSION}-brightgreen?style=flat-square&logo=android)](https://developer.android.com/about/versions) [![Target SDK](https://img.shields.io/badge/Target_SDK-${TARGET_SDK_VERSION}-blue?style=flat-square&logo=android)](https://developer.android.com/about/versions) [![Xposed API](https://img.shields.io/badge/Xposed_API-${XPOSED_API_VERSION}-orange?style=flat-square)](https://github.com/rovo89/XposedBridge) [![Telegram](https://img.shields.io/badge/Telegram-Group-2CA5E0?style=flat-square&logo=telegram&logoColor=white)](https://t.me/+NR2QaQ4dlEgxYmNl)"
 
 for readme in README.md README-EN.md; do
   tmp_file="$(mktemp)"
@@ -64,4 +71,4 @@ for readme in README.md README-EN.md; do
   mv "$tmp_file" "$readme"
 done
 
-echo "README badges synced from $TOML_FILE"
+echo "README badges synced from $TOML_FILE and gradle-wrapper.properties"
