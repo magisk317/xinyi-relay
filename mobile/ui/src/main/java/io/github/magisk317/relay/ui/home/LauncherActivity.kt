21575a284fa338e2584d4637696b1ae21e569fcc
magisk317 <code@mail.usdt.edu.kg>
2026-06-03 17:06:14 +0800

    feat(mobile): support launcher icon visibility toggle

--- mobile/ui/src/main/java/io/github/magisk317/relay/ui/home/LauncherActivity.kt
+++ mobile/ui/src/main/java/io/github/magisk317/relay/ui/home/LauncherActivity.kt
@@ -7,7 +7,8 @@ import android.os.Bundle
 class LauncherActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent = Intent(this, MainActivity::class.java).apply {
+        val intent = Intent(intent).apply {
+            setClass(this@LauncherActivity, MainActivity::class.java)
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
         }
         startActivity(intent)
