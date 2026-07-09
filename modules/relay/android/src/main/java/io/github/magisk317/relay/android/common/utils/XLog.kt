0bc8b18576805ba734f61d749f9e24d79c20b5cb
magisk317 <code@mail.usdt.edu.kg>
2026-07-10 01:52:56 +0800

    refactor(logging): adopt unified XLog from smscode-core:contract
    Update smscode-core submodule to 6a31a58. The local relay XLog
    becomes a thin delegate preserving the existing import path.

--- modules/relay/android/src/main/java/io/github/magisk317/relay/android/common/utils/XLog.kt
+++ modules/relay/android/src/main/java/io/github/magisk317/relay/android/common/utils/XLog.kt
@@ -1,77 +1,24 @@
 package io.github.magisk317.relay.android.common.utils
 
 import io.github.magisk317.smscode.runtime.contract.logging.LogRoute
+import io.github.magisk317.smscode.runtime.contract.logging.XLog as ContractXLog
 
+/**
+ * Thin delegate to the unified XLog in smscode-core:contract.
+ * Preserves `io.github.magisk317.relay.android.common.utils.XLog` import path.
+ */
 object XLog {
-
-    @JvmStatic
-    fun v(message: String, vararg args: Any?) {
-        RelayLogger.v(message, *args)
-    }
-
-    @JvmStatic
-    fun d(message: String, vararg args: Any?) {
-        RelayLogger.d(message, *args)
-    }
-
-    @JvmStatic
-    fun i(message: String, vararg args: Any?) {
-        RelayLogger.i(message, *args)
-    }
-
-    @JvmStatic
-    fun w(message: String, vararg args: Any?) {
-        RelayLogger.w(message, *args)
-    }
-
-    @JvmStatic
-    fun e(message: String, vararg args: Any?) {
-        RelayLogger.e(message, *args)
-    }
-
-    @JvmStatic
-    fun v(route: LogRoute, message: String, vararg args: Any?) {
-        RelayLogger.v(route, message, *args)
-    }
-
-    @JvmStatic
-    fun d(route: LogRoute, message: String, vararg args: Any?) {
-        RelayLogger.d(route, message, *args)
-    }
-
-    @JvmStatic
-    fun i(route: LogRoute, message: String, vararg args: Any?) {
-        RelayLogger.i(route, message, *args)
-    }
-
-    @JvmStatic
-    fun w(route: LogRoute, message: String, vararg args: Any?) {
-        RelayLogger.w(route, message, *args)
-    }
-
-    @JvmStatic
-    fun e(route: LogRoute, message: String, vararg args: Any?) {
-        RelayLogger.e(route, message, *args)
-    }
-
-    @JvmStatic
-    fun setLogLevel(logLevel: Int) {
-        RelayLogger.setLogLevel(logLevel)
-    }
-
-    @JvmStatic
-    fun getLogLevel(): Int = RelayLogger.getLogLevel()
-
-    @JvmStatic
-    fun setTestSink(sink: ((Int, String) -> Unit)?) {
-        RelayLogger.setTestSink(sink)
-    }
-
-    @JvmStatic
-    internal fun formatMessageForTest(
-        message: String,
-        args: Array<out Any?>,
-    ): String {
-        return RelayLogger.formatMessageForTest(message, args)
-    }
+    @JvmStatic fun v(message: String, vararg args: Any?) = ContractXLog.v(message, *args)
+    @JvmStatic fun d(message: String, vararg args: Any?) = ContractXLog.d(message, *args)
+    @JvmStatic fun i(message: String, vararg args: Any?) = ContractXLog.i(message, *args)
+    @JvmStatic fun w(message: String, vararg args: Any?) = ContractXLog.w(message, *args)
+    @JvmStatic fun e(message: String, vararg args: Any?) = ContractXLog.e(message, *args)
+    @JvmStatic fun v(route: LogRoute, message: String, vararg args: Any?) = ContractXLog.v(route, message, *args)
+    @JvmStatic fun d(route: LogRoute, message: String, vararg args: Any?) = ContractXLog.d(route, message, *args)
+    @JvmStatic fun i(route: LogRoute, message: String, vararg args: Any?) = ContractXLog.i(route, message, *args)
+    @JvmStatic fun w(route: LogRoute, message: String, vararg args: Any?) = ContractXLog.w(route, message, *args)
+    @JvmStatic fun e(route: LogRoute, message: String, vararg args: Any?) = ContractXLog.e(route, message, *args)
+    @JvmStatic fun setLogLevel(logLevel: Int) = ContractXLog.setLogLevel(logLevel)
+    @JvmStatic fun getLogLevel(): Int = ContractXLog.getLogLevel()
+    @JvmStatic fun setTestSink(sink: ((Int, String) -> Unit)?) = ContractXLog.setTestSink(sink)
 }
