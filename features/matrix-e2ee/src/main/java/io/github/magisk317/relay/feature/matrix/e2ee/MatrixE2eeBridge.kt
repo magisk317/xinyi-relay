d1595b42a6359fe3319d9360a0e5d5f47bd1c793
magisk317 <code@mail.usdt.edu.kg>
2026-07-16 12:41:37 +0800

    refactor(matrix): converge E2EE implementation

--- features/matrix-e2ee/src/main/java/io/github/magisk317/relay/feature/matrix/e2ee/MatrixE2eeBridge.kt
+++ features/matrix-e2ee/src/main/java/io/github/magisk317/relay/feature/matrix/e2ee/MatrixE2eeBridge.kt
@@ -1,8 +1,9 @@
 package io.github.magisk317.relay.feature.matrix.e2ee
 
 import android.content.Context
-import io.github.magisk317.relay.feature.matrix.e2ee.impl.MatrixE2eeFeatureSender
-import io.github.magisk317.relay.feature.matrix.e2ee.impl.MatrixE2eeFeatureVerification
+import io.github.magisk317.relay.matrix.e2ee.MatrixE2eePlatform
+import io.github.magisk317.relay.matrix.e2ee.MatrixE2eeRuntime
+import io.github.magisk317.relay.matrix.e2ee.MatrixE2eeVerificationRuntime
 import io.github.magisk317.relay.sender.MatrixE2eeSenderProvider
 import io.github.magisk317.relay.sender.MatrixE2eeVerificationProvider
 import io.github.magisk317.relay.sender.SLog
@@ -13,8 +14,9 @@ object MatrixE2eeBridge {
     @Suppress("UNUSED_PARAMETER")
     @JvmStatic
     fun install(context: Context) {
-        MatrixE2eeSenderProvider.install(MatrixE2eeFeatureSender)
-        MatrixE2eeVerificationProvider.install(MatrixE2eeFeatureVerification)
+        MatrixE2eePlatform.initialize()
+        MatrixE2eeSenderProvider.install(MatrixE2eeRuntime)
+        MatrixE2eeVerificationProvider.install(MatrixE2eeVerificationRuntime)
         SLog.i(TAG, "Matrix E2EE dynamic feature bridge installed")
     }
 
