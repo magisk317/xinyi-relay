magisk317/XposedSmsCode

feat(rules): add configurable resilient sources

core/src/main/java/com/github/magisk317/smscode/ui/smscoderule/SmsCodeRuleScreens.kt
@@ -22,6 +22,7 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ContentCopy
 import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -70,6 +71,7 @@ import io.github.magisk317.uikit.surface.DetailSectionCard
 import io.github.magisk317.uikit.surface.SectionHeading
 import io.github.magisk317.uikit.theme.UiKitStyle
 import io.github.magisk317.uikit.theme.currentUiKitStyle
+import io.github.magisk317.uikit.preference.UrlSourceSettingsScreen
 import java.util.regex.Pattern
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -100,18 +102,21 @@ fun SmsCodeRuleListScreen(
     onBack: () -> Unit,
     onAddClick: () -> Unit,
     onEditClick: (Long) -> Unit,
+    onSourceSettingsClick: () -> Unit,
 ) {
     when (currentUiKitStyle()) {
         UiKitStyle.Miuix -> SmsCodeRuleListScreenMiuix(
             onBack = onBack,
             onAddClick = onAddClick,
             onEditClick = onEditClick,
+            onSourceSettingsClick = onSourceSettingsClick,
         )
 
         UiKitStyle.Expressive -> SmsCodeRuleListScreenMaterial(
             onBack = onBack,
             onAddClick = onAddClick,
             onEditClick = onEditClick,
+            onSourceSettingsClick = onSourceSettingsClick,
         )
     }
 }
@@ -122,6 +127,7 @@ internal fun SmsCodeRuleListScreenShared(
     onBack: () -> Unit,
     onAddClick: () -> Unit,
     onEditClick: (Long) -> Unit,
+    onSourceSettingsClick: () -> Unit,
 ) {
     val context = LocalContext.current
     val storage = koinInject<UiStorageAccess>()
@@ -183,6 +189,12 @@ internal fun SmsCodeRuleListScreenShared(
                     }
                 },
                 actions = {
+                    IconButton(onClick = onSourceSettingsClick) {
+                        Icon(
+                            Icons.Filled.Settings,
+                            contentDescription = stringResource(id = R.string.action_rule_source_settings),
+                        )
+                    }
                     IconButton(
                         enabled = !officialLoading,
                         onClick = { loadOfficialRules(refresh = true) },
@@ -288,6 +300,23 @@ internal fun SmsCodeRuleListScreenShared(
             }
         }
     }
+}
+
+@Composable
+fun SmsCodeRuleSourceSettingsScreen(onBack: () -> Unit) {
+    val context = LocalContext.current
+    UrlSourceSettingsScreen(
+        title = stringResource(id = R.string.rule_source_settings_title),
+        fieldLabel = stringResource(id = R.string.rule_source_url_label),
+        supportingText = stringResource(id = R.string.rule_source_url_summary),
+        invalidUrlMessage = stringResource(id = R.string.rule_source_url_invalid),
+        savedMessage = stringResource(id = R.string.rule_source_saved),
+        saveFailedMessage = stringResource(id = R.string.rule_source_save_failed),
+        saveContentDescription = stringResource(id = R.string.action_save),
+        valueFlow = remember(context) { AppSmsCodeUtils.observeOfficialRuleSourceUrl(context) },
+        onSaveValue = { value -> AppSmsCodeUtils.saveOfficialRuleSourceUrl(context, value) },
+        onBack = onBack,
+    )
 }
 
 @Composable

core/src/main/java/com/github/magisk317/smscode/ui/home/MainScreen.kt
@@ -34,6 +34,7 @@ import com.github.magisk317.smscode.ui.nav.OverviewRoute
 import com.github.magisk317.smscode.ui.nav.RecordsRoute
 import com.github.magisk317.smscode.ui.nav.SettingsRoute
 import com.github.magisk317.smscode.ui.nav.SmsCodeRuleEditorRoute
+import com.github.magisk317.smscode.ui.nav.SmsCodeRuleSourceRoute
 import com.github.magisk317.smscode.ui.nav.SmsCodeRulesRoute
 import com.github.magisk317.smscode.ui.record.CodeRecordScreen
 import io.github.magisk317.uikit.surface.MainTabScaffold
@@ -55,7 +56,8 @@ private fun resolveTabIndex(destination: NavDestination?): Int {
         hierarchy.any { it.hasRoute(RecordsRoute::class) } -> 2
         hierarchy.any { it.hasRoute(SettingsRoute::class) } ||
             hierarchy.any { it.hasRoute(SmsCodeRulesRoute::class) } ||
-            hierarchy.any { it.hasRoute(SmsCodeRuleEditorRoute::class) } -> 3
+            hierarchy.any { it.hasRoute(SmsCodeRuleEditorRoute::class) } ||
+            hierarchy.any { it.hasRoute(SmsCodeRuleSourceRoute::class) } -> 3
         else -> 0
     }
 }
@@ -244,12 +246,20 @@ fun MainScreen(
                     onEditClick = { id ->
                         navController.navigate(SmsCodeRuleEditorRoute(id = id))
                     },
+                    onSourceSettingsClick = {
+                        navController.navigate(SmsCodeRuleSourceRoute)
+                    },
                 )
             }
             composable<SmsCodeRuleEditorRoute> { backStackEntry ->
                 val route = backStackEntry.toRoute<SmsCodeRuleEditorRoute>()
                 com.github.magisk317.smscode.ui.smscoderule.SmsCodeRuleEditorScreen(
                     ruleId = route.id,
+                    onBack = { navController.popBackStack() },
+                )
+            }
+            composable<SmsCodeRuleSourceRoute> {
+                com.github.magisk317.smscode.ui.smscoderule.SmsCodeRuleSourceSettingsScreen(
                     onBack = { navController.popBackStack() },
                 )
             }

magisk317/xinyi-relay

mobile/feature/rule/src/main/java/io/github/magisk317/relay/ui/smscoderule/SmsCodeRuleScreens.kt
@@ -24,6 +24,7 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ContentCopy
 import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -62,6 +63,7 @@ import io.github.magisk317.smscode.domain.model.BuiltinSmsCodeRules
 import io.github.magisk317.smscode.runtime.common.rules.OfficialSmsCodeRule
 import io.github.magisk317.smscode.runtime.common.rules.SmsCodeRuleCatalogSnapshot
 import io.github.magisk317.uikit.surface.chromeTopAppBarColors
+import io.github.magisk317.uikit.preference.UrlSourceSettingsScreen
 import org.koin.compose.koinInject
 import java.util.regex.Pattern
 import kotlinx.coroutines.Dispatchers
@@ -93,6 +95,7 @@ fun SmsCodeRuleListScreen(
     onBack: () -> Unit,
     onAddClick: () -> Unit,
     onEditClick: (Long) -> Unit,
+    onSourceSettingsClick: () -> Unit,
 ) {
     val context = LocalContext.current
     val repository: AppConfigRepository = koinInject()
@@ -153,6 +156,12 @@ fun SmsCodeRuleListScreen(
                     }
                 },
                 actions = {
+                    IconButton(onClick = onSourceSettingsClick) {
+                        Icon(
+                            Icons.Filled.Settings,
+                            contentDescription = stringResource(id = R.string.action_rule_source_settings),
+                        )
+                    }
                     IconButton(
                         enabled = !officialLoading,
                         onClick = { loadOfficialRules(refresh = true) },
@@ -260,6 +269,23 @@ fun SmsCodeRuleListScreen(
             }
         }
     }
+}
+
+@Composable
+fun SmsCodeRuleSourceSettingsScreen(onBack: () -> Unit) {
+    val context = LocalContext.current
+    UrlSourceSettingsScreen(
+        title = stringResource(id = R.string.rule_source_settings_title),
+        fieldLabel = stringResource(id = R.string.rule_source_url_label),
+        supportingText = stringResource(id = R.string.rule_source_url_summary),
+        invalidUrlMessage = stringResource(id = R.string.rule_source_url_invalid),
+        savedMessage = stringResource(id = R.string.rule_source_saved),
+        saveFailedMessage = stringResource(id = R.string.rule_source_save_failed),
+        saveContentDescription = stringResource(id = R.string.save),
+        valueFlow = remember(context) { RelaySmsCodeUtils.observeOfficialRuleSourceUrl(context) },
+        onSaveValue = { value -> RelaySmsCodeUtils.saveOfficialRuleSourceUrl(context, value) },
+        onBack = onBack,
+    )
 }
 
 @Composable

mobile/ui/src/main/java/io/github/magisk317/relay/ui/home/MainScreen.kt
@@ -87,6 +87,7 @@ import io.github.magisk317.relay.ui.nav.SenderTypeRoute
 import io.github.magisk317.relay.ui.nav.SendersRoute
 import io.github.magisk317.relay.ui.nav.SettingsRoute
 import io.github.magisk317.relay.ui.nav.SmsCodeRuleEditorRoute
+import io.github.magisk317.relay.ui.nav.SmsCodeRuleSourceRoute
 import io.github.magisk317.relay.ui.nav.SmsCodeRulesRoute
 import io.github.magisk317.relay.ui.nav.VerificationSettingsRoute
 import io.github.magisk317.relay.ui.record.BlacklistHitListScreen
@@ -219,6 +220,8 @@ fun MainScreen(
                 sectionFromOrigin(entry.toRoute<SmsCodeRulesRoute>().origin)
             destination.hasRoute(SmsCodeRuleEditorRoute::class) ->
                 sectionFromOrigin(entry.toRoute<SmsCodeRuleEditorRoute>().origin)
+            destination.hasRoute(SmsCodeRuleSourceRoute::class) ->
+                sectionFromOrigin(entry.toRoute<SmsCodeRuleSourceRoute>().origin)
             destination.hasRoute(CloudBackupRoute::class) -> NavigationSection.SETTINGS
             destination.hasRoute(GlobalForwardFilterRoute::class) ->
                 sectionFromOrigin(entry.toRoute<GlobalForwardFilterRoute>().origin)
@@ -696,12 +699,20 @@ fun MainScreen(
                                     SmsCodeRuleEditorRoute(id = id, origin = route.origin),
                                 )
                             },
+                            onSourceSettingsClick = {
+                                navController.navigate(SmsCodeRuleSourceRoute(origin = route.origin))
+                            },
                         )
                     }
                     composable<SmsCodeRuleEditorRoute> { backStackEntry ->
                         val route = backStackEntry.toRoute<SmsCodeRuleEditorRoute>()
                         io.github.magisk317.relay.ui.smscoderule.SmsCodeRuleEditorScreen(
                             ruleId = route.id,
+                            onBack = { navController.popBackStack() },
+                        )
+                    }
+                    composable<SmsCodeRuleSourceRoute> {
+                        io.github.magisk317.relay.ui.smscoderule.SmsCodeRuleSourceSettingsScreen(
                             onBack = { navController.popBackStack() },
                         )
                     }