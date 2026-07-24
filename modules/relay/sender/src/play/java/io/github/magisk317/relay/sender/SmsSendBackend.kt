7f5e37a67df5bcfaf0bf2381e55e50f7b6fcf189
magisk317 <code@mail.usdt.edu.kg>
2026-07-24 22:16:10 +0800

    feat(otel): mark play SMS send backend as unsupported flavor

--- modules/relay/sender/src/play/java/io/github/magisk317/relay/sender/SmsSendBackend.kt
+++ modules/relay/sender/src/play/java/io/github/magisk317/relay/sender/SmsSendBackend.kt
@@ -2,6 +2,7 @@ package io.github.magisk317.relay.sender
 
 import android.content.Context
 import io.github.magisk317.relay.sender.config.SmsSetting
+import io.github.magisk317.xposed.logging.MagiskOtel
 
 internal object SmsSendBackend {
     fun send(
@@ -11,6 +12,22 @@ internal object SmsSendBackend {
         content: String,
         waitForSentResult: Boolean,
     ) {
+        MagiskOtel.event(
+            name = "sms.forward",
+            attributes = mapOf(
+                "result" to "error",
+                "duration_ms" to "0",
+                "process" to "app",
+                "stage" to "sms_send_backend",
+                "reason" to "unsupported_flavor",
+                "sender_type" to "sms",
+                "target_count" to mobiles.size.toString(),
+                "content_length" to content.length.toString(),
+                "wait_for_result" to waitForSentResult.toString(),
+                "sim_slot" to setting.simSlot.toString(),
+            ),
+            statusOk = false,
+        )
         throw UnsupportedOperationException("当前发行版不支持短信发送")
     }
 }
