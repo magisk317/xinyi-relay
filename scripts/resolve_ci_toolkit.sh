f6b8e031e93f2e5178d35d1171d319cfcfd281b8
magisk317 <code@mail.usdt.edu.kg>
2026-08-07 16:00:25 +0800

    ci: advance toolkit pin

--- scripts/resolve_ci_toolkit.sh
+++ scripts/resolve_ci_toolkit.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 TOOLKIT_DIR="${MAGISK_CI_TOOLKIT_DIR:-${ROOT_DIR}/.magisk-ci-toolkit}"
 TOOLKIT_REPOSITORY="${MAGISK_CI_TOOLKIT_REPOSITORY:-https://gitlab.com/magisk3171/magisk-ci-toolkit.git}"
-TOOLKIT_REF="${MAGISK_CI_TOOLKIT_REF:-2df77420a6f05d42283a5ada14957b62746a17aa}"
+TOOLKIT_REF="${MAGISK_CI_TOOLKIT_REF:-60989c7c1a03b2bc0e86299ee1709f842ac3fb12}"
 TOOLKIT_REPOSITORY_AUTH="$TOOLKIT_REPOSITORY"
 if [[ -n "${CI_JOB_TOKEN:-}" && "$TOOLKIT_REPOSITORY" == https://gitlab.com/* ]]; then
   TOOLKIT_REPOSITORY_AUTH="${TOOLKIT_REPOSITORY/https:\/\//https:\/\/gitlab-ci-token:${CI_JOB_TOKEN}@}"
