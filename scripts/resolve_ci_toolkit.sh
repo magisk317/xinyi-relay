d7856bcf118217c806b49f0e35cd6409c4ae1712
magisk317 <code@mail.usdt.edu.kg>
2026-08-26 17:39:05 +0800

    chore(ci): pin reusable toolkit commit

--- scripts/resolve_ci_toolkit.sh
+++ scripts/resolve_ci_toolkit.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 TOOLKIT_DIR="${MAGISK_CI_TOOLKIT_DIR:-${ROOT_DIR}/.magisk-ci-toolkit}"
 TOOLKIT_REPOSITORY="${MAGISK_CI_TOOLKIT_REPOSITORY:-https://gitlab.com/magisk3171/magisk-ci-toolkit.git}"
-TOOLKIT_REF="${MAGISK_CI_TOOLKIT_REF:-1d4e81b29086f6e037b3ed9d3f888dfce75f46f5}"
+TOOLKIT_REF="${MAGISK_CI_TOOLKIT_REF:-e4c4a9105025ee4d3ba6d97733850134dc5dac37}"
 TOOLKIT_REPOSITORY_AUTH="$TOOLKIT_REPOSITORY"
 if [[ -n "${CI_JOB_TOKEN:-}" && "$TOOLKIT_REPOSITORY" == https://gitlab.com/* ]]; then
   TOOLKIT_REPOSITORY_AUTH="${TOOLKIT_REPOSITORY/https:\/\//https:\/\/gitlab-ci-token:${CI_JOB_TOKEN}@}"
